b46616ff36ce37a26bc9c56734024d153ca4fbb0
Florian <Florian@DESKTOP-AVU9PSR>
2021-03-02 15:09:03 +0100

    anfang statistik Seite

--- app/src/main/java/com/example/bottlecollector/MainActivity.java
+++ app/src/main/java/com/example/bottlecollector/MainActivity.java
@@ -1,35 +1,27 @@
 package com.example.bottlecollector;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static  final  String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public int highScore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Context context = this;
-        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(getString(R.string.saved_high_score_key), 20);
-        editor.apply();
-        int highScore = sharedPref.getInt(getString(R.string.saved_high_score_key),0);
-        TextView text = (TextView) findViewById(R.id.keyAusgabe);
-        text.setText(highScore+" ");
+        StatisikSpeicher.setGeganeneKilometer(this, 30);
 
         //Liste holen und anzeigen
         List<String> erg = Belohnung.belohnungeng(1200, 200);
@@ -37,4 +29,12 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.ausgabe);
         listView.setAdapter(adapter);
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, StatistikActivity.class);
+        String message = StatisikSpeicher.getGegangeneKilometer(this);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
