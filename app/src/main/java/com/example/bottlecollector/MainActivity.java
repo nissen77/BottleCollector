8b1d3b8d1940d02fca35580f34c9048fafe4e832
Florian <Florian@DESKTOP-AVU9PSR>
2021-03-02 14:03:11 +0100

    save key value getestet

--- app/src/main/java/com/example/bottlecollector/MainActivity.java
+++ app/src/main/java/com/example/bottlecollector/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.bottlecollector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
@@ -16,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = this;
+        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getString(R.string.saved_high_score_key), 20);
+        editor.apply();
+        int highScore = sharedPref.getInt(getString(R.string.saved_high_score_key),0);
+        TextView text = (TextView) findViewById(R.id.keyAusgabe);
+        text.setText(highScore+" ");
+
+        //Liste holen und anzeigen
         List<String> erg = Belohnung.belohnungeng(1200, 200);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, erg);
         ListView listView = (ListView) findViewById(R.id.ausgabe);
