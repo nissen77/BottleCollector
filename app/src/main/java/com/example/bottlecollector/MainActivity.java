968df1d3ae9995d2ac78f12dae28772d862a7482
Florian <Florian@DESKTOP-AVU9PSR>
2021-03-09 15:24:30 +0100

    weiter arbeit an statistik Seite

--- app/src/main/java/com/example/bottlecollector/MainActivity.java
+++ app/src/main/java/com/example/bottlecollector/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.icu.util.LocaleData;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
 import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +25,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        StatisikSpeicher.setGeganeneKilometer(this, 30);
+        LocalDate date = LocalDate.now();
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        int woche =  date.get(weekFields.weekOfWeekBasedYear());
+
+
+        StatisikSpeicher.setGeganeneMeterGesamt(this, 30);
+        StatisikSpeicher.setBesterTag(this, 8342, LocalDate.of(2010, 03, 10));
+        StatisikSpeicher.setBesteWoche(this, 1000, woche);
 
         //Liste holen und anzeigen
         List<String> erg = Belohnung.belohnungeng(1200, 200);
@@ -32,8 +43,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view){
         Intent intent = new Intent(this, StatistikActivity.class);
-        String message = StatisikSpeicher.getGegangeneKilometer(this);
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
